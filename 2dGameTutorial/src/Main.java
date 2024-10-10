8f289166e75b3b23533b9eca6981da16d914a649
David-Gabriel Dediu <135978285+Davidd0605@users.noreply.github.com>
2024-10-10 10:08:03 +0200

    FIXED SOME STUFF

--- 2dGameTutorial/src/Main.java
+++ 2dGameTutorial/src/Main.java
@@ -2,6 +2,7 @@
 import javax.swing.*;
 import java.awt.*;
 
+
 /**
  *
  * @author David
@@ -14,9 +15,22 @@ public class Main {
         //Create a frame for our game
 
         JFrame frame = new JFrame();
+
+        try {
+            for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                    if(info.getName().equals("CDE/Motif")) {
+                        UIManager.setLookAndFeel(info.getClassName());
+                        break;
+                    }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //change look and feel of main Jframe
         frame.setSize(24 * 32, 600);
         frame.setResizable(false);
-        frame.setTitle("Touch grass");
+        frame.setTitle("FPS survivor");
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
